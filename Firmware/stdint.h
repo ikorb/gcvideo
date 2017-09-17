207462009b6361c62dff8abd6ccab896a50c310b
Ingo Korb <ingo@akana.de>
2017-09-17 22:58:39 +0200

    Fix int8_t definition

--- Firmware/stdint.h
+++ Firmware/stdint.h
@@ -36,8 +36,8 @@ typedef unsigned int   uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char  uint8_t;
 
-typedef int   int32_t;
-typedef short int16_t;
-typedef char  int8_t;
+typedef int         int32_t;
+typedef short       int16_t;
+typedef signed char int8_t;
 
 #endif
