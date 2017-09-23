9d6a9d3718edd27246b03e0f19c961dc61e47333
Ingo Korb <ingo@akana.de>
2017-09-23 19:03:54 +0200

    Add a script to build and zip all binaries for GCVideo-DVI

---
+++ HDL/gcvideo_dvi/build-all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+function build () {
+    local TARGET="$1"
+
+    rm -rf build
+    make TARGET=$TARGET
+
+    zip -9jX binaries/gcvideo-$VERSION-$TARGET.zip \
+        build/gcvideo-dvi-$TARGET-$VERSION-M25P40.xsvf \
+        build/gcvideo-dvi-$TARGET-$VERSION-spirom.cfi \
+        build/gcvideo-dvi-$TARGET-$VERSION-spirom.mcs \
+        build/gcvideo-dvi-$TARGET-$VERSION.bit
+}
+
+if [ ! -d codegens ]; then
+    echo "You seem to be running this from the wrong directory."
+    exit 1
+fi
+
+VERSION=`make printversion`
+
+mkdir binaries
+
+build p2xh-gc
+build p2xh-wii
+build shuriken-gc
+build shuriken-wii
+build shuriken-v3-gc
+build shuriken-v3-wii
+build dual-gc
+build dual-wii
+
+rm -rf build
