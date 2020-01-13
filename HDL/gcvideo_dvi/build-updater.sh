27c3866b0ede8ced51f585548a99e0c9d4c55e6c
Ingo Korb <ingo@akana.de>
2020-01-14 00:29:25 +0100

    don't put Gamecube-only firmware in the Wii updater.dol

--- HDL/gcvideo_dvi/build-updater.sh
+++ HDL/gcvideo_dvi/build-updater.sh
@@ -23,14 +23,10 @@ fi
                          build/main-shuriken-v3-gc/toplevel_shuriken.tagmain \
                          build/main-shuriken-v3-wii/toplevel_shuriken.tagmain \
                          build/main-dual-gc/toplevel_dual.tagmain \
-                         build/main-dual-wii/toplevel_dual.tagmain \
+                         build/main-dual-wii/toplevel_dual.tagmain
 
 ./scripts/buildupdate.pl $UPDATER_WII binaries/updater-$VERSION-wii.dol \
-                         build/main-p2xh-gc/toplevel_p2xh.tagmain \
                          build/main-p2xh-wii/toplevel_p2xh.tagmain \
-                         build/main-shuriken-gc/toplevel_shuriken.tagmain \
                          build/main-shuriken-wii/toplevel_shuriken.tagmain \
-                         build/main-shuriken-v3-gc/toplevel_shuriken.tagmain \
                          build/main-shuriken-v3-wii/toplevel_shuriken.tagmain \
-                         build/main-dual-gc/toplevel_dual.tagmain \
-                         build/main-dual-wii/toplevel_dual.tagmain \
+                         build/main-dual-wii/toplevel_dual.tagmain
